6ce8b3d84f978139b7b0dca2176df8cbc057e719
Bernardo Ramos Ferreira <733712@sga.pucminas.br>
2026-09-08 22:19:57 -0300

    Criação de classes prática carrinho de compras

---
+++ Pratica02/src/praticaCarrinhoCompras/Fatura.java
@@ -0,0 +1,45 @@
+package praticaCarrinhoCompras;
+
+import java.util.ArrayList;
+
+public class Fatura {
+    private ArrayList<Item> itens;
+    private double valorTotalFatura;
+
+    public Fatura(ArrayList<Item> itens, double valorTotalFatura){
+        this.itens = new ArrayList<Item>();
+        this.valorTotalFatura = valorTotalFatura;
+    }
+
+    public ArrayList<Item> getItens() {
+        return itens;
+    }
+
+    public void setItens(ArrayList<Item> itens) {
+        this.itens = itens;
+    }
+
+    public double getValorTotalFatura() {
+        return valorTotalFatura;
+    }
+
+    public void setValorTotalFatura(double valorTotalFatura) {
+        this.valorTotalFatura = valorTotalFatura;
+    }
+
+    public double calcularValorFatura(){
+        this.valorTotalFatura = 0.0;
+        for(int i = 0; i < this.itens.size(); i++){
+            this.valorTotalFatura += this.itens.get(i).getvalorTotalItem();
+        }
+        return this.valorTotalFatura;
+    }
+
+    public void adicionarItem(Item item){
+        this.itens.add(item);
+    }
+
+    public void realizarCompra(){
+        System.out.println("Compra realizada!\nValor total da compra: " + "R$" + this.valorTotalFatura);
+    }
+}
---
+++ Pratica02/src/praticaCarrinhoCompras/Item.java
@@ -0,0 +1,39 @@
+package praticaCarrinhoCompras;
+
+public class Item {
+    private Produto produtoComprado;
+    private int qntComprada;
+    private double valorTotalItem;
+
+    public Item(Produto produtoComprado, int qntComprada, double valorTotal){
+        this.produtoComprado = produtoComprado;
+        this.qntComprada = qntComprada;
+        this.valorTotalItem = produtoComprado.getPreco() * qntComprada; 
+    }
+
+    public Produto getProdutoComprado() {
+        return produtoComprado;
+    }
+
+    public void setProdutoComprado(Produto produtoComprado) {
+        this.produtoComprado = produtoComprado;
+    }
+
+    public int getQntComprada() {
+        return qntComprada;
+    }
+
+    public void setQntComprada(int qntComprada) {
+        this.qntComprada = qntComprada;
+    }
+
+    public double getvalorTotalItem() {
+        return valorTotalItem;
+    }
+
+    public void setvalorTotalItem(double valorTotalItem) {
+        this.valorTotalItem = valorTotalItem;
+    }
+
+    
+}
